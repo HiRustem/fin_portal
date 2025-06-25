fcb4a726d7ffae9aff9b00fbf5512700ecdca3e6
Rustem <rustem_hajrullin@enot.work>
2025-06-25 20:43:09 +0300

    fix: theme toggle

--- netlify/functions/remix.js
+++
@@ -1,8 +0,0 @@
-const { createRequestHandler } = require('@remix-run/netlify');
-
-const build = require('../../netlify/entry.server');
-
-exports.handler = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV,
-});
